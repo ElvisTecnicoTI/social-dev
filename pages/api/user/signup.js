993eeaac9b570f57d82ebc63946d9439e0b8392f
elvis_tecnico_ti <elvistecnico_ti@protonmail.com>
2023-02-11 15:55:37 -0300

    criei o middleware para a conexão com o banco de dados

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,9 +1,9 @@
-import connect from 'next-connect'
 import Joi from 'joi'
+import createHandler from '../../../lib/middlewares/nextConnect'
 import validate from '../../../lib/middlewares/validation'
 
 import { signupUser } from '../../../modules/user/user.service'
-
+  
 const postSchema = Joi.object({
   firstName: Joi.string().required().max(50),
   lastName: Joi.string().required().max(50),
@@ -12,8 +12,9 @@ const postSchema = Joi.object({
   password: Joi.string().required().max(50).min(6),
 }) 
 
-const signup = connect()
-  .post(validate({ body: postSchema }), (req, res) => {
+const signup = createHandler()
+
+  signup.post(validate({ body: postSchema }), (req, res) => {
     signupUser(req.body)
     res.status(200).json({ teste: "ok" })
   })
